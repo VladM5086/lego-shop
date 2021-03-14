f75492e899d74e096f15d469d7d58f68a47511d0
Student <student@corp.ies.org.ua>
2021-03-14 16:48:59 +0300

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,22 @@
+let cartBody = document.getElementById('cart');
+window.addEventListener('click', (event) => {
+console.log('click');
+
+if (event.target.classList.contains('button-buy')) {
+	console.log('клик на кнопку');
+	addItems(event.target);
+ }
+});
+function addItems(element){
+
+console.log('додавання товару');
+
+let li = document.createElement('li');
+
+li.innerHTML = element.dataset.img
+             + element.dataset.name
+             + element.dataset.price
+             + '<div class="delete-items"> X </div>';
+
+ cartBody.appendChild(li);
+}
